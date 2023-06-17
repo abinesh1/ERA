cf77405264a941965b9dc476a7b3e5682a4197fe
Abinesh Sankar <abinesh1@outlook.com>
2023-06-16 22:24:17 -0400

    Adding files #1

---
+++ S7/model.py
@@ -0,0 +1,201 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torchsummary
+
+
+class Base(nn.Module):
+    def summary(self, input_size=None):
+        return torchsummary.summary(
+            self,
+            input_size=input_size
+        )
+
+
+class Model1(Base):
+    def __init__(self):
+        super(Model1, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, padding=1, bias=False),
+            nn.ReLU(),
+        )
+
+        self.transition1 = nn.Sequential(nn.MaxPool2d(2, 2))
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, padding=1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, padding=1, bias=False),
+            nn.ReLU(),
+        )
+
+        self.transition2 = nn.Sequential(nn.MaxPool2d(2, 2))
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, padding=1, bias=False),
+            nn.ReLU(),
+        )
+
+        self.output = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(128, 256, 1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(256, 10, 1),
+            nn.Flatten(),
+            nn.LogSoftmax(-1),
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.transition1(x)
+        x = self.conv2(x)
+        x = self.transition2(x)
+        x = self.conv3(x)
+        x = self.output(x)
+        return x
+
+
+class Model2(Base):
+    def __init__(self):
+        super(Model2, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 10, 3, padding=1, bias=False),
+            nn.ReLU()
+        )
+
+        self.transition1 = nn.Sequential(nn.MaxPool2d(2, 2))
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(10, 16, 3, padding=1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(16, 16, 3, padding=1, bias=False),
+            nn.ReLU(),
+        )
+
+        self.transition2 = nn.Sequential(nn.MaxPool2d(2, 2))
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(16, 16, 3, padding=1, bias=False),
+            nn.ReLU(),
+        )
+
+        self.output = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(16, 32, 1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(32, 10, 1),
+            nn.Flatten(),
+            nn.LogSoftmax(-1),
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.transition1(x)
+        x = self.conv2(x)
+        x = self.transition2(x)
+        x = self.conv3(x)
+        x = self.output(x)
+        return x
+
+
+class Model3(Base):
+    def __init__(self):
+        super(Model3, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 10, 3, padding=1, bias=False),
+            nn.ReLU()
+        )
+
+        self.transition1 = nn.Sequential(nn.MaxPool2d(2, 2))
+
+        self.conv2 = nn.Sequential(
+            nn.BatchNorm2d(10),
+            nn.Conv2d(10, 16, 3, padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Conv2d(16, 16, 3, padding=1, bias=False),
+            nn.ReLU(),
+        )
+
+        self.transition2 = nn.Sequential(nn.MaxPool2d(2, 2))
+
+        self.conv3 = nn.Sequential(
+            nn.BatchNorm2d(16),
+            nn.Conv2d(16, 16, 3, padding=1, bias=False),
+            nn.ReLU(),
+        )
+
+        self.output = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.BatchNorm2d(16),
+            nn.Conv2d(16, 32, 1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 10, 1),
+            nn.Flatten(),
+            nn.LogSoftmax(-1),
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.transition1(x)
+        x = self.conv2(x)
+        x = self.transition2(x)
+        x = self.conv3(x)
+        x = self.output(x)
+        return x
+
+
+class Model4(Base):
+    def __init__(self):
+        DROP = 0.01
+        super(Model4, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 9, 3, padding=1, bias=False),
+            nn.Dropout(DROP),
+            nn.ReLU(),
+            nn.BatchNorm2d(9),
+            nn.Conv2d(9, 10, 3, padding=1, bias=False),
+            nn.Dropout(DROP),
+            nn.ReLU(),
+        )
+
+        self.transition1 = nn.Sequential(nn.MaxPool2d(2, 2))
+
+        self.conv2 = nn.Sequential(
+            nn.BatchNorm2d(10),
+            nn.Conv2d(10, 16, 3, padding=1, bias=False),
+            nn.Dropout(DROP),
+            nn.ReLU()
+        )
+
+        self.transition2 = nn.Sequential(nn.MaxPool2d(2, 2))
+
+        self.conv3 = nn.Sequential(
+            nn.BatchNorm2d(16),
+            nn.Conv2d(16, 16, 3, padding=1, bias=False),
+            nn.Dropout(DROP),
+            nn.ReLU(),
+        )
+
+        self.output = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.BatchNorm2d(16),
+            nn.Conv2d(16, 32, 1, bias=False),
+            nn.Dropout(DROP),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 10, 1),
+            nn.Flatten(),
+            nn.LogSoftmax(-1),
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.transition1(x)
+        x = self.conv2(x)
+        x = self.transition2(x)
+        x = self.conv3(x)
+        x = self.output(x)
+        return x
